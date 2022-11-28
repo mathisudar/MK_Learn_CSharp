bfb4d2f4e20413d6987d35a8459f0f551e8d6f5b
MATHI <76192299+mathisudar@users.noreply.github.com>
2022-11-28 16:32:16 +0530

    Create W49_2022_CS_0002_WriteLine.cs

---
+++ W49_2022_CS_0002_WriteLine.cs
@@ -0,0 +1,15 @@
+class WriteLineApp
+{
+    static void Main()
+    {
+        System.Console.WriteLine("Welcome ");
+        System.Console.WriteLine("To ");
+        System.Console.WriteLine("C-Sharp Programming!");
+
+        System.Console.ReadKey();
+        System.Console.Clear();
+        System.Console.WriteLine("Thank You!");
+        System.Console.ReadKey();
+
+    }
+}
