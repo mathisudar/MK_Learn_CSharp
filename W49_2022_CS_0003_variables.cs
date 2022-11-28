49423a4401578747799e8b15db8b581ec4ee9e1f
MATHI <76192299+mathisudar@users.noreply.github.com>
2022-11-28 22:51:07 +0530

    Create W49_2022_CS_0003_variables.cs

---
+++ W49_2022_CS_0003_variables.cs
@@ -0,0 +1,16 @@
+class variablesApp
+{
+    static void Main()
+    {
+        int age = 40;
+        string name = "Jhon";
+
+        System.Console.Write("Hey,");
+        System.Console.Write(name);
+        System.Console.Write("- your age is ");
+        System.Console.Write(age);
+        System.Console.ReadKey();
+
+
+    }
+}
