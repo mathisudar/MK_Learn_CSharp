52d9006b0696836c5d0786372fcc83b79eb968f8
MATHI <76192299+mathisudar@users.noreply.github.com>
2022-11-29 22:08:42 +0530

    Create W49_2022_CS_0006_areaCircle.cs

---
+++ W49_2022_CS_0006_areaCircle.cs
@@ -0,0 +1,13 @@
+class AreaCircle
+{
+    static void Main()
+    {
+        decimal circleRad = 10M;
+        decimal PI = 3.14159M;
+        decimal areaCircle = PI * circleRad * circleRad;
+
+        System.Console.WriteLine("Area - Circle:"+areaCircle);
+        System.Console.ReadLine();
+
+    }
+}
