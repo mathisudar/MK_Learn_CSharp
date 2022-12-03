1875b32137b1fd8a7fa0797a40052061630a6006
MATHI <76192299+mathisudar@users.noreply.github.com>
2022-12-03 18:19:20 +0530

    Create CS_0009_IF_ELSE.cs

---
+++ CS_0009_IF_ELSE.cs
@@ -0,0 +1,19 @@
+class ifElseClass
+{
+    static void Main()
+    {
+        int marks = 75;
+
+        if (marks >= 35)
+        {
+            System.Console.WriteLine("Congratulation! - PASS!");
+        }
+        else
+        {
+            System.Console.WriteLine("Better luck next time! - FAIL!");
+        }
+
+        System.Console.ReadKey();
+
+    }
+}
